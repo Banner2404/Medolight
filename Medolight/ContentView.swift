ba3a0359527b9d6a48b97001a656fd8c8fea0f93
Eugene Sobol <esobol@insoftgroup.com>
2019-11-19 19:38:45 +0300

    Initial Commit

---
+++ Medolight/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Medolight
+//
+//  Created by Евгений Соболь on 11/19/19.
+//  Copyright © 2019 Esobol. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
